d8608e2656539f71d4c96b5adfac1c170a7e412e
MatthewSpiteri04 <matthew.spiteri04@gmail.com>
2024-03-22 20:18:56 +0100

    restructuring

---
+++ Backend/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/User/Test")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { };
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# OnlineLibrary
-Implementing an online library system which allows users to access all types of written documents at one location.
