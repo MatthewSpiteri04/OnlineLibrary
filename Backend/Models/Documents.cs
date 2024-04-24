e00d7255c35fec81ab244dc0aa64381be714d31e
MatthewSpiteri04 <matthew.spiteri04@gmail.com>
2024-04-24 17:23:03 +0200

    Added Library And Favourties System

---
+++ Backend/Models/Documents.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.Models
+{
+	public class Documents
+	{
+		public int Id { get; set; }
+		public string Category { get; set; }
+		public string Title { get; set; }
+		public string Language { get; set; }
+		public DateTime UploadDate { get; set; }
+		public bool PublicAccess { get; set; }
+		public string DocumentLocation { get; set; }
+		public bool IsFavourite { get; set; }
+	}
+}
---
+++ Backend/Models/FvouriteRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Models
+{
+    public class FavouriteRequest
+    {
+        public int DocumentId { get; set; }
+        public int UserId { get; set; }
+        public bool IsFavourite { get; set; }
+    }
+}
